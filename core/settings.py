cc79f301cf11d8e55cfccadfe4c6b6e5d19083ce
0MoShang0 <10756032@ntub.edu.tw>
2022-05-16 20:22:17 +0800

    make it better

--- core/settings.py
+++ core/settings.py
@@ -30,7 +30,7 @@ SECRET_KEY = 'django-insecure-0n537xapk)821+*%ea3jo7zz@dxxg$x=h^%3c!q2(9!ol3$@h)
 # ALLOWED_HOSTS = []
 
 # 在Heroku用的連線資訊
-DEBUG = True
+DEBUG = False
 ALLOWED_HOSTS = ['learn-it-well-estartup-api.herokuapp.com']
 
 
