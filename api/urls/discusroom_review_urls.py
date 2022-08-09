ad265477c6ff8b1666abcd80c2538335a27cdc59
Yuki <10756044@ntub.edu.tw>
2022-08-09 21:33:57 +0800

    修改-AutoField

--- api/urls/discusroom_review_urls.py
+++ api/urls/discusroom_review_urls.py
@@ -9,6 +9,7 @@ urlpatterns = [
     path('all/', get_all_reviews),
     path('rec/', rec_reviews),
     path('addroom/',addroom),
+    path('addroom_subject/',get_subject_reviews),
 
     # 下面test/ 供討論室相關表格進行測試
     path('test/', get_all_reviews_test),
