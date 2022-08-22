4957d4d2b16b01a35463284bb040a97dede4a6ec
0MoShang0 <10756032@ntub.edu.tw>
2022-08-22 22:44:22 +0800

    修改-自習室加入到對應房間

--- api/urls/subject_review_urls.py
+++ api/urls/subject_review_urls.py
@@ -6,6 +6,6 @@ from api.views.subject_review_views import *
 app_name = 'subject'
 
 urlpatterns = [
-    path('all/', get_all_reviews),  # 列表
+    path('all/<int:pk>', get_all_reviews),  # 列表
 
 ]
--- api/views/subject_review_views.py
+++ api/views/subject_review_views.py
@@ -5,7 +5,7 @@ from rest_framework import status
 from rest_framework.decorators import api_view
 from rest_framework.response import Response
 
-from api.models import Subject
+from api.models import Subject, Studyroom
 
 # 每一個測試的api_view,一次只能取消註解一個
 
@@ -14,17 +14,31 @@ from utils.decorators import user_login_required
 
 @api_view()
 @user_login_required
-def get_all_reviews(request):
+def get_all_reviews(request, pk):
     subjects = Subject.objects.all()
-
+    try:
+        studyroom = Studyroom.objects.get(pk=pk)
+    except:
+        return Response({'success': False, 'message': '查無此房間'}, status=status.HTTP_404_NOT_FOUND)
     return Response({
         'success': True,
-        'data': [
+        'data':
             {
-                'no': subject.pk,
-                'name': subject.name,
-            }
-            for subject in subjects
+                # 自習室
+                'no': studyroom.pk,
+                # 'schoolsys_no': discussroom.schoolsys_no.pk,
+                # 'subject_no': discussroom.subject_no.pk,
+                'study_name': studyroom.name,
+                # 'pwd': discussroom.pwd,
+                # 'total_people': discussroom.total_people,
+                'study_sub_names': [
+                    {  # 科目
+                        'sub_no': subject.no,
+                        'sub_name': subject.name,
+                    }
+                    for subject in subjects
+                ]
 
-        ]
+            }
+
     })
