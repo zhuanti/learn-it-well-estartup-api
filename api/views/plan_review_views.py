6746694dd640d25114acaef1b78eb2c9422be3a6
Tynt-170 <10756022@ntub.edu.tw>
2022-10-24 23:00:26 +0800

    修改-讀書規劃

--- api/views/plan_review_views.py
+++ api/views/plan_review_views.py
@@ -15,15 +15,15 @@ from utils.decorators import user_login_required
 @user_login_required
 def addplan(request):
     data = request.data
+    # data = request.query_params
     user_id = request.session['user_id']
     # 新增
     try:
         Plan.objects.create(
             user_id=user_id,
             name=data['name'],
-            pace=0,
-            # 0為未完成,1為完成
-            datetime=datetime.datetime.now())
+            pace=0)
+        # 0為未完成,1為完成
 
         return Response({'success': True, 'message': '新增成功'})
 
@@ -38,12 +38,12 @@ def editplan(request):
     data = request.data
 
     no = data.get('no')
-    plans = Plan.objects.filter(no=no)
+    plans = Plan.objects.filter(pk=no)
     if not plans.exists():
         return Response({'success': False, 'message': '沒有此讀書規劃'}, status=status.HTTP_404_NOT_FOUND)
 
     try:
-        plans.update(name=data['name'], pace=data['pace'], datetime=data['datetime'])
+        plans.update(name=data['name'])
         return Response({'success': True, 'message': '編輯成功'})
     except:
         return Response({'success': False, 'message': '編輯失敗'}, status=status.HTTP_400_BAD_REQUEST)
@@ -54,7 +54,7 @@ def editplan(request):
 @user_login_required
 def get_all_reviews_test(request):
     # plans = Plan.objects.all()
-    #data = request.data
+    # data = request.data
     data = request.query_params
 
     user_id = data.get('user_id')
