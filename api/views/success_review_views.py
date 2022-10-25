e31132c9fea26b80d641e83f40386170fe5fb1fc
0MoShang0 <10756032@ntub.edu.tw>
2022-10-25 16:16:06 +0800

    修改-成就列表資料抓取

--- api/views/success_review_views.py
+++ api/views/success_review_views.py
@@ -30,47 +30,31 @@ from utils.decorators import user_login_required
 @api_view()
 @user_login_required
 def get_all_reviews(request):
-    data = request.data
+    data = request.query_params
     user_id = data.get('user_id')
-    # user_id=request.session['user_id']
-    # user = User.objects.filter(pk=user_id)
-    success_list = Success_list.objects.all()
-    successs = Success.objects.all()
-    print(user_id)
+
+    success_lists = Success_list.objects.filter(user_id=user_id)
+
     return Response({
         'success': True,
         'data': [
             {
-                'no': success.pk,
-                'name': success.name,
-                'pace': success.pace,
-                'success_list': [
+                'no': success_list.pk,
+                'user_id': success_list.user.pk,
+                'success_no': success_list.success_no.pk,
+                'pace': success_list.pace,
+                'ockif': success_list.lockif,
+                # 'no': success.pk,
+                # 'name': success.name,
+                # 'pace': success.pace,
+                'success_name': [
                     {
-                        'success_list_no': success_list.pk,
-                        'user_id': success_list.user.pk,
-                        'success_no': success_list.success_no,
-                        'pace': success_list.pace,
-                        'lockif': success_list.lockif,
+                        'success_name': success.name,
+                        'success_pace': success.pace,
                     }
-                    for success_list in Success_list.objects.filter(pk=user_id)
-                    # for success_list in Success_list.objects.all()
+                    for success in Success.objects.filter(pk=success_list.success_no.pk)
                 ],
-
-                #     'no': success_list.pk,
-                #     'user_id': success_list.user.pk,
-                #     'success': [
-                #         {
-                #             'success_no': success.pk,
-                #             'name': success.name,
-                #             'pace': success.pace,
-                #         }
-                #         for success in Success.objects.filter(no=success_list.pk)
-                #     ],
-                #     'pace': success_list.pace,
-                #     'lockif': success_list.lockif
-                # }
-                # for success_list in success_lists
             }
-            for success in successs
+            for success_list in success_lists
         ]
     })
