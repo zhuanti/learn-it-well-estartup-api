be44fefd5192ee89de7c49b749e2c24fdf6c5f8e
Tynt-170 <10756022@ntub.edu.tw>
2022-10-29 10:51:56 +0800

    註解-讀書規劃

--- api/urls/plan_review_urls.py
+++ api/urls/plan_review_urls.py
@@ -2,18 +2,17 @@ from django.urls import path
 
 from api.views.plan_review_views import *
 
-
 app_name = 'plan'
 
 urlpatterns = [
     # path('all/', get_all_reviews),
-    
+
     # 下面test/ 供成就相關表格進行測試
-    path('get/', get_all_reviews_test),
-    path('add/', addplan),
-    path('showedit/', showeditplan),
-    path('editplan/', editplan), # 編輯
-    path('delete/', deleteplan),
+    path('get/', get_all_reviews_test),  # 顯示讀書規劃
+    path('add/', addplan),  # 新增讀書規劃
+    path('showedit/', showeditplan),  # 顯示特定讀書規劃
+    path('editplan/', editplan),  # 編輯讀書規劃
+    path('delete/', deleteplan),  # 刪除讀書規劃
 
     # 學姊的範例測試
     # path('get/<int:pk>/', get_review),
