73693d2b9ac0db02299a195d3442c81f7ff28ef5
Tynt-170 <10756022@ntub.edu.tw>
2022-12-13 21:43:48 +0800

    新增-修改密碼

--- api/urls/user_review_urls.py
+++ api/urls/user_review_urls.py
@@ -9,9 +9,10 @@ urlpatterns = [
     # path('all/', get_all_reviews),
 
     # 下面test/ 供成就相關表格進行測試
-    path('test/', get_user_detail_test), #測試
+    path('test/', get_user_detail_test),  # 測試
     path('detail/', get_user_detail),  # 顯示使用者個人資訊
     path('detail/edit/', user_detail_edit),  # 編輯使用者個人資訊
+    path('pwd/edit/', user_pwd_edit),  # 編輯使用者密碼
 
     # 學姊的範例測試
     # path('get/<int:pk>/', get_review),
